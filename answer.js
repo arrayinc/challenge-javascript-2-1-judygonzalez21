d43872f26590568f0945dc7ab848cc1e4d7bf7d5
Judy Gonzalez <judy@thearrayschool.com>
2018-12-05 14:06:14 -0700

    Not sure if I answered it correctly?

--- answer.js
+++ answer.js
@@ -1,1 +1,10 @@
 // *** YOUR ANSWER BELOW ***
+function solveEquation(x){
+  let power = x * x;
+  let multiple = 5 * x;
+  let solutionForF = (power - multiple + 6) / x;
+  return solutionForF * x;
+
+}
+
+solveEquation(2);
